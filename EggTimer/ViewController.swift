8771247ba31af862ed85c11d7302fb08609b1983
xpy <cpiwarski01@gmail.com>
2020-07-27 07:32:07 -0700

    meta: Timer completed

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,37 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
-    var secondsRemaining: Int = 0
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    let alarm = "alarm_sound"
+    var totalTime: Int = 0
+    var secondsPassed: Int = 0
     var timer = Timer()
+    var player: AVAudioPlayer!
     
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var progressbar: UIProgressView!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
+        // Reset timer variables for button press
+        progressbar.progress = 0.0
+        secondsPassed = 0
         timer.invalidate()
-        progressbar.progress = 1.0
         guard let hardness = sender.currentTitle else { return }
         timerLabel.text = "Cooking eggs..."
         
-        secondsRemaining = eggTimes[hardness]!
         
+        totalTime = eggTimes[hardness]!
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
 
     }
     
     @objc func updateTimer() {
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) left")
-            secondsRemaining -= 1
+        if secondsPassed < totalTime {
+            print("\(secondsPassed) seconds passed")
+            secondsPassed += 1
+            progressbar.progress = Float(secondsPassed) / Float(totalTime)
         } else {
             timer.invalidate()
             timerLabel.text = "Done!"
+            guard let url: URL = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+            
+            player = try! AVAudioPlayer(contentsOf: url)
+            
+            player.play()
+
         }
     }
-    
 }
